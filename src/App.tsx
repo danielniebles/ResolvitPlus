d3756f9d987b56a30a29a088cb880d656256c62a
danielniebles <danielf.niebles@gmail.com>
2022-11-01 23:48:48 -0500

    feat: add routing to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import MovieCard from "./components/MovieCard";
+import Home from "./pages/Home";
+import { Route } from "wouter";
+import AdvancedSearch from "./pages/AdvancedSearch";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <MovieCard />
-    </div>
+    <>
+      <Route component={Home} path="/home" />
+      <Route component={AdvancedSearch} path="/search" />
+    </>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import Cover from "../sections/Cover";
+import Popular from "../sections/Popular";
+import Movies from "../sections/Movies";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Cover />
+      <Popular />
+      <Movies />
+    </>
+  );
+};
+
+export default Home;
